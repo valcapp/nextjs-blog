5bab0d88fc42b1206ca032db9f8d5631745d5c82
valcapp <valcapp-srf@users.noreply.github.com>
2020-11-24 22:14:34 +0000

    fixed import

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,7 +2,7 @@ import Layout from 'components/layout'
 import Head from 'next/head'
 
 import { getAllPostIds, getPostData } from 'lib/posts'
-import Date from 'components/Date'
+import Date from 'components/date'
 
 import utilStyles from 'styles/utils.module.css'
 
